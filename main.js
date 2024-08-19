ef2c791a60570635effea8a9c97c488c164c4818
FES001 <firdavs7794948@gmail.com>
2024-08-19 22:14:56 +0500

    Working on the functionality of this form.

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const emailAddress = document.getElementById('emailAddress');
 const generalEnquiry = document.getElementById('generalEnquiry');
 const supportRequest = document.getElementById('supportRequest');
 const messageInput = document.getElementById('messageInput');
-const checkbox = document.getElementById('checkbox').checked;
+const checkbox = document.getElementById('checkbox');
 
 
 // REQUIRED TEXTS
@@ -20,8 +20,49 @@ const queryTypeRequiredText = document.getElementById('queryTypeRequiredText');
 const messageRequiredText = document.getElementById('messageRequiredText');
 const checkboxRequiredText = document.getElementById('checkboxRequiredText');
 
-submitBtn.addEventListener('submit', e => {
+submitBtn.addEventListener('click', e => {
+    const errorMessages = [];
+
+    if (firstName.value.length === 0) {
+        errorMessages.push('The firstName field is empty');
+        firstNameRequiredText.textContent = 'This field is required';
+        firstName.style.border = '1px solid hsl(0, 66%, 54%)'; 
+    } else {
+        firstNameRequiredText.textContent = '';
+        firstName.style.border = '1px solid hsl(186, 15%, 59%)'; 
+    };
 
     
+    if (lastName.value.length === 0) {
+        errorMessages.push('The lastName field is empty');
+        lastNameRequiredText.textContent = 'This field is required';
+        lastName.style.border = '1px solid hsl(0, 66%, 54%)';
+    } else {
+        lastNameRequiredText.textContent = '';
+        lastName.style.border = '1px solid hsl(186, 15%, 59%)';
+    };
 
+    if (emailAddress.value.length === 0) {
+        errorMessages.push('The emailAddress field is empty');
+        emailAddressRequiredText.textContent = 'Please enter a valid email address';
+        emailAddress.style.border = '1px solid hsl(0, 66%, 54%)';
+    } else {
+        emailAddressRequiredText.textContent = '';
+        emailAddress.style.border = '1px solid hsl(186, 15%, 59%)';
+    };
+
+    if (messageInput.value.length === 0) {
+        errorMessages.push('The messageInput field is empty');
+        messageRequiredText.textContent = 'This field is required';
+        messageInput.style.border = '1px solid hsl(0, 66%, 54%)';
+    } else {
+        messageRequiredText.textContent = '';
+        messageInput.style.border = '1px solid hsl(186, 15%, 59%)';
+    };
+
+    if (errorMessages.length > 0) {
+        e.preventDefault();
+    };
+
+    console.log(errorMessages);
 });

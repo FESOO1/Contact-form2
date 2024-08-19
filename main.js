4f1cc726ecf430c48097ce40583527b060607f78
FES001 <firdavs7794948@gmail.com>
2024-08-19 21:57:33 +0500

    Update main.js

---
+++ main.js
@@ -0,0 +1,27 @@
+const submitBtn = document.getElementById('submitButton');
+
+// INPUTS
+
+const firstName = document.getElementById('firstName');
+const lastName = document.getElementById('lastName');
+const emailAddress = document.getElementById('emailAddress');
+const generalEnquiry = document.getElementById('generalEnquiry');
+const supportRequest = document.getElementById('supportRequest');
+const messageInput = document.getElementById('messageInput');
+const checkbox = document.getElementById('checkbox').checked;
+
+
+// REQUIRED TEXTS
+
+const firstNameRequiredText = document.getElementById('firstNameRequiredText');
+const lastNameRequiredText = document.getElementById('lastNameRequiredText');
+const emailAddressRequiredText = document.getElementById('emailAddressRequiredText');
+const queryTypeRequiredText = document.getElementById('queryTypeRequiredText');
+const messageRequiredText = document.getElementById('messageRequiredText');
+const checkboxRequiredText = document.getElementById('checkboxRequiredText');
+
+submitBtn.addEventListener('submit', e => {
+
+    
+
+});
